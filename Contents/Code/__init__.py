359f124d9d24c615bb00a6d47f4b65424847df14
Florian Winkler <flo@smasher.at>
2013-02-07 15:38:31 +0100

    Untermenüs werden automatisch richtig befüllt

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -35,7 +35,6 @@ def MainMenu():
     oc = ObjectContainer()
 
     for item in HTML.ElementFromURL(ROOT_URL).xpath('//ul[@id="list_shows"]/li'):
-    	Log("URL = http://atv.at" + item.xpath('./a[1]/@href')[0])
 	#Log("THUMB = " + item.xpath('./a[1]/img[1]/@src')[0])
 	#Log("TITLE = " + item.xpath('./a[2]/h3[1]/text()')[0])
 	#Log("TAGLINE = " + item.xpath('./a[3]/em[1]/text()')[0])
@@ -43,7 +42,6 @@ def MainMenu():
 
 	oc.add(DirectoryObject(
 		key = Callback(EpisodeMenu, url="http://atv.at" + item.xpath('./a[1]/@href')[0]),
-		#url = "http://atv.at" + item.xpath('./a[1]/@href')[0],
 		title =  item.xpath('./a[2]/h3[1]/text()')[0],
 		tagline = item.xpath('./a[3]/em[1]/text()')[0],
 		summary = item.xpath('./a[3]/text()[2]')[0].strip(),
@@ -57,7 +55,7 @@ def EpisodeMenu(url):
 
     oc = ObjectContainer()
 
-    Log("Entering EpisodeMenu with URL: " + url)
+    #Log("Entering EpisodeMenu with URL: " + url)
     page = getUrl(url)
     id1 = re.compile('contentset_id%22%3A(.+?)%', re.DOTALL).findall(page)
     id2 = re.compile('active_playlist_id%22%3A(.+?)%', re.DOTALL).findall(page)
@@ -65,27 +63,29 @@ def EpisodeMenu(url):
     newurl = "http://atv.at/player_playlist_page_json/" + id1[0] + "/" + id2[0] + "/1"
     json = JSON.ObjectFromURL(newurl)
     numberofitems = json['items_count']
-    total_page_count = json['total_page_count']
+    total_page_count = int(json['total_page_count'])
 
-    for idx, item in enumerate(json['1']):
-	id = item['id']
-	oc.add(EpisodeObject(
-	    url = newurl + "/" + str(idx),
-	    title = item['title'] + " | " + item['subtitle'],
-	    summary = item['description'],
-	    thumb = item['thumbnail_url'],
-	    art = item['image_url'],
-	    absolute_index = int(item['keyValueEpisode']),
-	    season = int(item['keyValueSeason']),
+    for index in range(1,total_page_count+1):
+	if index == 1:
+	    data = json #beacuse we've already fetched it before
+	else:
+	    newurl = "http://atv.at/player_playlist_page_json/" + id1[0] + "/" + id2[0] + "/" + str(index)
+	    data =  JSON.ObjectFromURL(newurl)
+
+        for idx, item in enumerate(data[str(index)]):
+	    oc.add(EpisodeObject(
+	        url = newurl + "/" + str(idx),
+	    	title = item['title'] + " | " + item['subtitle'],
+	    	summary = item['description'].replace("[br]", "\n"),
+	    	thumb = item['thumbnail_url'],
+	    	art = item['image_url'],
+	    	absolute_index = int(item['keyValueEpisode']),
+	    	season = int(item['keyValueSeason']),
+	    	)
 	    )
-	)
 	    
     return oc 
 		
-
-def ChannelMenu(sender, channel = None):
-    return MessageContainer("test", "test" + str(channel))
-
 def getUrl(url):
     req = urllib2.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:16.0) Gecko/20100101 Firefox/16.0')
